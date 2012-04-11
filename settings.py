663fe6cdd4116876c0fa7df5f72794813f30b617
Jan Javorek <honza@javorek.net>
2012-04-11 21:26:15 +0200

    spacing

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*- #
+
 
 # Author & site
 AUTHOR = u'Honza Javorek'
@@ -7,15 +7,18 @@ SITENAME = u'Javorové lístky'
 SITEURL = 'http://www.honzajavorek.cz'
 PATH = 'posts'
 
+
 # Timezone, language
 TIMEZONE = 'Europe/Prague'
 LOCALE = ('cs_CZ.utf8', 'en_GB.utf8')
 DEFAULT_LANG='cs'
 DEFAULT_DATE_FORMAT = '%x'
 
+
 # External services
 DISQUS_SITENAME = 'javorove-listky'
 GOOGLE_ANALYTICS = ''
+
 
 # Blog settings
 REVERSE_TAG_ORDER = True
@@ -24,27 +27,33 @@ DEFAULT_PAGINATION = 5
 DEFAULT_CATEGORY = 'blog'
 MD_EXTENSIONS = ['codehilite', 'extra']
 
+
 # Menu
 MENUITEMS = (
     (u'Řehoř', 'http://www.example.com'),
 )
 
+
 # Static paths will be copied under the same name
 #STATIC_PATHS = ('css', 'images')
+
 
 # A list of files to copy from the source to the destination
 #FILES_TO_COPY = (
 #    ('extra/robots.txt', 'robots.txt'),
 #)
 
+
 # Feeds
 CATEGORY_FEED = 'feeds/%s.xml'
 TAG_FEED = 'feeds/%s.xml'
 FEED_MAX_ITEMS = 10
 
+
 # Tags
 TAG_CLOUD_STEPS = 4
 TAG_CLOUD_MAX_ITEMS = 50
+
 
 # Blogroll
 LINKS =  (
@@ -53,6 +62,7 @@ LINKS =  (
     ('Jinja2', 'http://jinja.pocoo.org'),
     ('You can modify those links in your config file', '#')
 )
+
 
 # Social widget
 SOCIAL = (
