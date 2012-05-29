2c2d82146bfb69024e9af0e7600601456727910f
Jan Javorek <honza@javorek.net>
2012-05-29 17:18:53 +0200

    deploying changes in pages

---
+++ theme/js/main.js
@@ -0,0 +1,37 @@
+/**
+ * Main JavaScript.
+ */
+
+$(document).ready(function() {
+
+    if ($('.article').length) {
+        // poem styling
+        $('#content article > p > br').closest('p').addClass('poem');
+
+        // anchors
+        $('h1, h2, h3, h4', '.article').filter('[id]').each(function() {
+            var $heading = $(this);
+            var $a = $('<a>', {'href': '#' + $heading.attr('id'), text: '#'});
+            $a.appendTo($heading);
+            $a.wrap($('<small>', {'class': 'anchor'}));
+        });
+
+        // disqus
+        var check_disqus = setInterval(function() {
+            $reactionsHeader = $('.dsq-h3-reactions', '#dsq-content');
+            var disqus_ready = $('#dsq-reply').get(0) != null;
+            if (disqus_ready) {
+                clearInterval(check_disqus); // remove timer
+
+                // my changes
+                $('.dsq-h3-reactions', '#dsq-content').text('Reakce');
+
+                // display comments
+                $('.article hr').show();
+                $('#comments_inner').show();
+                $('#comments').removeClass('loading');
+            }
+        }, 100);
+    }
+
+});
