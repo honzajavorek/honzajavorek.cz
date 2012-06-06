daf2990947d7f61249fd3f856afba5d33363cff5
Jan Javorek <honza@javorek.net>
2012-06-06 19:05:17 +0200

    bug in redirects, fixed redirecting of feeds etc.

--- migration/redirects/index.php
+++ migration/redirects/index.php
@@ -1,17 +1,17 @@
 <?php
-$uri = $_SERVER['REQUEST_URI'];
+$uri = rtrim($_SERVER['REQUEST_URI'], '/');
 
-if ($uri == '/feeds/') {
+if ($uri == '/feeds' || $uri == '/feed') {
     $loc = 'http://feeds.feedburner.com/javorove-listky';
 
-} elseif ($uri == '/') {
-    $loc = rtrim('http://honzajavorek.cz');
-
-} elseif ($uri == '/comments/feed/') {
+} elseif ($uri == '/comments/feed' || $uri == '/comments/feeds') {
     $loc = 'http://javorove-listky.disqus.com/latest.rss';
 
+} elseif (!$uri) {
+    $loc = rtrim('http://honzajavorek.cz');
+
 } else {
-    $loc = rtrim('http://honzajavorek.cz/blog' . $uri, '/');
+    $loc = 'http://honzajavorek.cz/blog' . $uri;
 }
 
 header('HTTP/1.1 301 Moved Permanently');
