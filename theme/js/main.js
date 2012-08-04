d4b5924ad7378888f8706435b6d74d5d79098131
Honza Javorek <jan.javorek@gmail.com>
2012-08-04 22:34:34 +0200

    deploying changes in pages

--- theme/js/main.js
+++ theme/js/main.js
@@ -15,27 +15,17 @@ $(document).ready(function() {
             $a.appendTo($heading);
             $a.wrap($('<small>', {'class': 'anchor'}));
         });
-
-        // disqus
-        // var check_disqus = setInterval(function() {
-        //     $reactionsHeader = $('.dsq-h3-reactions', '#dsq-content');
-        //     var disqus_ready = $('#dsq-reply').get(0) !== null;
-        //     if (disqus_ready) {
-        //         clearInterval(check_disqus); // remove timer
-
-        //         // prepare context for lookup
-        //         $disqus = $('#dsq-content');
-
-        //         // my changes
-        //         $('.dsq-h3-reactions', $disqus).text('Reakce');
-
-        //         // display comments & ensure the right scroll position
-        //         $('html').addClass('ready');
-        //         if (window.location.hash) {
-        //             window.location.hash = window.location.hash;
-        //         }
-        //     }
-        // }, 100);
     }
 
 });
+
+if ($('#disqus_thread').length) {
+    // disqus loading
+    var check_disqus = setInterval(function() {
+        var disqus_ready = $('#disqus_thread').height() > 100;
+        if (disqus_ready) {
+            clearInterval(check_disqus); // remove timer
+            $('html').addClass('ready');
+        }
+    }, 100);
+};
