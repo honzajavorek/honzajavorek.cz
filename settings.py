337541b328d5cd504b3dd75efffa5bd1128ac3d8
Honza Javorek <jan.javorek@gmail.com>
2013-04-08 14:40:48 +0200

    Publishing.

--- settings.py
+++ settings.py
@@ -2,11 +2,14 @@
 
 
 import re
+import os
 import json
 import urllib
-import requests
-from jinja2 import Markup
 from datetime import date, datetime, timedelta
+
+import requests
+from PIL import Image
+from jinja2 import Markup
 
 
 # Author & site
@@ -64,6 +67,7 @@ FEEDBURNER_SITENAME = 'javorove-listky'
 # Theming
 THEME = 'theme'
 THEME_STATIC_PATHS = ('static',)
+IMAGE_MAX_WIDTH = 650
 
 DISQUS_SITENAME = 'javorove-listky'
 DISQUS_SITENAME_EXPEDITIONS = 'javorove-expedice'
@@ -91,13 +95,47 @@ def to_datetime(dt):
     return dt
 
 
-def figure(html):
-    html = re.sub(r'(<iframe[^\>]*>[^\<]*</iframe>)',
-                  r'<figure>\1</figure>', html)
-    html = re.sub(r'(<object[^\>]*>.*</object>)',
-                  r'<figure>\1</figure>', html)
-    return re.sub(r'<p([^\>]*)>\s*(<img[^\>]*>)\s*</p>',
-                  r'<figure\1>\2</figure>', html)
+class FigureCreator(object):
+
+    dirname = os.path.abspath(os.path.dirname(__file__))
+
+    def iframe_to_figure(self, html):
+        return re.sub(ur'(<iframe[^\>]*>[^\<]*</iframe>)',
+                      ur'<figure>\1</figure>', html)
+
+    def object_to_figure(self, html):
+        return re.sub(ur'(<object[^\>]*>.*</object>)',
+                      ur'<figure>\1</figure>', html)
+
+    def _replace_image(self, match):
+        figure_attrs = match.group(1)
+        img_tag = match.group(2)
+
+        match = re.search(ur'src="([^"]+)', img_tag)
+        if match:
+            # '../static/images/something.png'
+            path = re.sub('(../)?static/', 'posts/', match.group(1))
+            filename = os.path.join(self.dirname, path)
+            width = Image.open(filename).size[0]
+            if width > IMAGE_MAX_WIDTH:
+                width = IMAGE_MAX_WIDTH
+            attr = 'width="{0}"'.format(int(width))
+
+            # inject width
+            img_tag = re.sub(ur'\s*width="[^"]+"\s*', r' ', img_tag)
+            img_tag = re.sub(ur'\s*>', ' {0}>'.format(attr), img_tag)
+
+        return u'<figure{0}>{1}</figure>'.format(figure_attrs, img_tag)
+
+    def image_to_figure(self, html):
+        return re.sub(ur'<p([^\>]*)>\s*(<img[^\>]*>)\s*</p>',
+                      self._replace_image, html)
+
+    def __call__(self, html):
+        html = self.iframe_to_figure(html)
+        html = self.object_to_figure(html)
+        html = self.image_to_figure(html)
+        return html
 
 
 def code(html):
@@ -231,7 +269,7 @@ class MapCreator(object):
             venues += self.path_venue.format(place=place_name)
         return url + self.path_param.format(venues=venues)
 
-    def __call__(self, places, width=650, height=400):
+    def __call__(self, places, width=IMAGE_MAX_WIDTH, height=400):
         url = self.url.format(
             width=width,
             height=height,
@@ -286,7 +324,7 @@ def split_people(people):
 
 
 JINJA_FILTERS = {
-    'figure': figure,
+    'figure': FigureCreator(),
     'code': code,
     'month_name': month_name,
     'format_date': format_date,
