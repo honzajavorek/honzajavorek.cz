246322013ebff0b03f2672fd42cc9a25d4a98273
Honza Javorek <mail@honzajavorek.cz>
2018-12-06 10:20:39 +0100

    add the .circleci config to the gh-pages branch

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,18 @@
 
 set -e
 
+# blog
 cd blog
 pipenv run deploy
 cd ..
 
+# home
 cd home
 pipenv run deploy
-cp -r ./node_modules/fork-awesome/fonts ../_output/static/  # hotfix
 cd ..
 
+# hotfixes
+cp -r ./home/node_modules/fork-awesome/fonts ./_output/static/
+cp -r ./.circleci ./_output/
 ghp-import -m 'deploying' ./_output/
 git push origin gh-pages:gh-pages --force
