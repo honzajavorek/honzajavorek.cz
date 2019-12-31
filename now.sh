e37de2fd73b2566f7f622c192896e85989454a8a
Honza Javorek <mail@honzajavorek.cz>
2019-12-31 11:15:15 +0100

    deal with unnecessary files later

--- now.sh
+++ now.sh
@@ -5,10 +5,3 @@ python3 -m venv ./venv
 
 rm -r ./content/drafts
 ./venv/bin/pelican --settings=publishconf.py --debug --fatal warnings
-
-rm -r \
-    ./output/theme/fork-awesome/less \
-    ./output/theme/fork-awesome/scss \
-    ./output/theme/fork-awesome/css/v5-compat*.* \
-    ./output/theme/fork-awesome/*.md \
-    ./output/theme/fork-awesome/*.json
