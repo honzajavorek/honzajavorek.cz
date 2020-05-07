a7889b030a1eb456d2a16f9e807058e635bee594
Honza Javorek <mail@honzajavorek.cz>
2020-05-07 22:12:26 +0200

    migrate to github pages

--- publishconf.py
+++ publishconf.py
@@ -1,7 +1,5 @@
 import os
 import sys
-from operator import itemgetter
-import logging
 
 import requests
 
@@ -11,6 +9,4 @@ from pelicanconf import *
 
 DELETE_OUTPUT_DIRECTORY = True
 GOOGLE_ANALYTICS = 'UA-1316071-6'
-
-SITEURL = os.getenv('VERCEL_URL', 'https://honzajavorek.cz')
-logging.getLogger(__name__).info('SITEURL=%s', SITEURL)
+SITEURL = 'https://honzajavorek.cz'
