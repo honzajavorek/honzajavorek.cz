06c01fda040a76442c5a579abcd0ac43d4ef61e2
Honza Javorek <mail@honzajavorek.cz>
2022-11-18 12:40:34 +0100

    use publish config

--- telegram.py
+++ telegram.py
@@ -6,10 +6,10 @@ from pathlib import Path
 import requests
 
 sys.path.append(os.curdir)
-import pelicanconf
+import publishconf
 
 
-CONTENT_PATH = Path(pelicanconf.PATH)
+CONTENT_PATH = Path(publishconf.PATH)
 ARTICLE_TITLE_RE = re.compile(r'Title:\s+(?P<title>[^\n]+)\s*')
 METADATA_RE = re.compile(r'(?P<metadata>([\w\s\-]+:\s+[^\n]+\n)+)')
 TELEGRAM_COMMENTS_KEY = 'Telegram-Comments'
@@ -25,7 +25,7 @@ print(f"Last article is {article_path}")
 if TELEGRAM_COMMENTS_KEY not in article_metadata:
     print(f"Article doesn't seem to have Telegram comments")
     article_slug = article_path.stem[11:]
-    article_url = f"{pelicanconf.SITEURL}/{pelicanconf.ARTICLE_URL.format(slug=article_slug)}"
+    article_url = f"{publishconf.SITEURL}/{publishconf.ARTICLE_URL.format(slug=article_slug)}"
     article_title = ARTICLE_TITLE_RE.search(article_text).group('title')
 
     print(f"Posting {article_url} to Telegram")
