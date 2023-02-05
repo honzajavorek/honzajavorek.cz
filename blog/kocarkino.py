d6f8fa053b311c86442c96173478340d7ab78066
Honza Javorek <mail@honzajavorek.cz>
2023-02-06 00:37:43 +0100

    support baby bio

--- blog/kocarkino.py
+++ blog/kocarkino.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from urllib.parse import quote
 from zoneinfo import ZoneInfo
 
+import extruct
 import click
 from icalendar import Calendar, Event
 import requests
@@ -13,19 +14,18 @@ HEADING_PREFIX = 'Kočárkino: '
 
 @click.command()
 def main():
-    response = requests.get('https://kcvozovna.cz/program/')
-    response.raise_for_status()
-    html_tree = html.fromstring(response.content)
-
     calendar = Calendar()
     calendar.add('prodid', '-//kocarkino//honzajavorek.cz//')
     calendar.add('version', '2.0')
 
-    for event in html_tree.cssselect('[data-date]'):
+    response = requests.get('https://kcvozovna.cz/program/')
+    response.raise_for_status()
+    html_tree = html.fromstring(response.content)
+    for row in html_tree.cssselect('[data-date]'):
         starts_at = datetime \
-            .fromisoformat(event.get('data-date')) \
+            .fromisoformat(row.get('data-date')) \
             .replace(tzinfo=ZoneInfo('Europe/Prague'))
-        link = event.cssselect('h2 a')[0]
+        link = row.cssselect('h2 a')[0]
         url = link.get('href')
 
         heading = link.text_content().strip()
@@ -34,11 +34,28 @@ def main():
             csfd_url = f'https://www.csfd.cz/hledat/?q={quote(title)}'
 
             event = Event()
-            event.add('summary', heading)
-            event.add('description', '\n'.join([title, url, csfd_url]))
+            event.add('summary', f'Kočárkino: {title}')
+            event.add('description', '\n'.join([url, csfd_url]))
             event.add('dtstart', starts_at)
             event.add('duration', timedelta(hours=2))
-            event.add('location', 'KC VOZOVNA, Koněvova 2687/164, 13000 Praha 3')
+            event.add('location', 'KC VOZOVNA, Koněvova 2687/164, Praha 3')
             calendar.add_component(event)
+
+    response = requests.post('https://www.biooko.net/api_program', data={
+        'cinema[]': 2,
+        'cycle[]': 'baby-bio',
+        '_locale': 'cs',
+    })
+    response.raise_for_status()
+    for data in extruct.extract(response.text)['json-ld']:
+        csfd_url = f"https://www.csfd.cz/hledat/?q={quote(data['name'])}"
+
+        event = Event()
+        event.add('summary', f"Baby Bio: {data['name']}")
+        event.add('description', '\n'.join([data['url'], csfd_url]))
+        event.add('dtstart', datetime.fromisoformat(data['startDate']))
+        event.add('duration', timedelta(hours=2))
+        event.add('location', 'Bio Oko, Františka Křížka 460/15, Praha 7')
+        calendar.add_component(event)
 
     click.echo(calendar.to_ical())
