260e0011c827653aab9def9ae086c2b978013f31
Honza Javorek <mail@honzajavorek.cz>
2023-03-26 15:43:10 +0200

    generate feed

--- blog/reading.py
+++ blog/reading.py
@@ -1,19 +1,28 @@
+from pprint import pprint
+
 import click
 from notion_client import Client
 from notion_client.helpers import iterate_paginated_api
-from itertools import chain
 
-from pprint import pprint
+from feedgen.feed import FeedGenerator
+
+
+FEED_ID = 'EVVcNQWvn9.QMwn6Z--KMTk4rmJnvNyABKRCcGM@'
 
 
 @click.command()
 @click.argument('notion_token', envvar='NOTION_TOKEN')
 @click.option('--database-id', default='6300c201957f499a81ebc9a097950fc8')
 def main(notion_token, database_id):
+    feed = FeedGenerator()
+    feed.id(FEED_ID)
+    feed.title("HJ's Reading")
     notion = Client(auth=notion_token)
     for result in iterate_paginated_api(notion.databases.query, database_id=database_id):
         for item in result:
-            # TODO
-            pprint(item['created_time'])
-            pprint(item['properties']['Name']['title'][0]['plain_text'])
-            pprint(item['properties']['URL']['url'])
+            entry = feed.add_entry()
+            entry.id('http://lernfunk.de/media/654321')
+            entry.title(item['properties']['Name']['title'][0]['plain_text'])
+            entry.link(href=item['properties']['URL']['url'])
+            entry.published(item['created_time'])
+    click.echo(feed.atom_str())
