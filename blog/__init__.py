7dc6b66873bb6d68f57f652d5fb4f19985c5f684
Honza Javorek <mail@honzajavorek.cz>
2024-02-03 11:55:59 +0100

    split reading and watching

--- blog/__init__.py
+++ blog/__init__.py
@@ -19,7 +19,7 @@ from blog.toots import main as toots
 from blog.weeknotes import main as weeknotes
 from blog.media import main as media
 from blog.kocarkino import main as kocarkino
-from blog.reading import main as reading
+from blog.notion import main as notion
 from blog.update import main as update
 from blog.wait import main as wait
 
@@ -29,44 +29,55 @@ def main():
     pass
 
 
-main.add_command(kocarkino, 'kocarkino')
-main.add_command(mastodon, 'mastodon')
-main.add_command(media, 'media')
-main.add_command(reading, 'reading')
-main.add_command(strava, 'strava')
-main.add_command(telegram, 'telegram')
-main.add_command(toots, 'toots')
-main.add_command(update, 'update')
-main.add_command(wait, 'wait')
-main.add_command(weeknotes, 'weeknotes')
+main.add_command(kocarkino, "kocarkino")
+main.add_command(mastodon, "mastodon")
+main.add_command(media, "media")
+main.add_command(notion, "notion")
+main.add_command(strava, "strava")
+main.add_command(telegram, "telegram")
+main.add_command(toots, "toots")
+main.add_command(update, "update")
+main.add_command(wait, "wait")
+main.add_command(weeknotes, "weeknotes")
 
 
 @main.command()
-@click.option('--path', 'content_path', default='content', type=click.Path(exists=True))
-@click.option('--settings-module', default='publishconf', type=importlib.import_module)
-@click.option('--debug/--no-debug', default=True)
+@click.option("--path", "content_path", default="content", type=click.Path(exists=True))
+@click.option("--settings-module", default="publishconf", type=importlib.import_module)
+@click.option("--debug/--no-debug", default=True)
 def build(content_path, settings_module, debug):
     extra_args = []
 
     if debug:
-        extra_args.append('--debug')
+        extra_args.append("--debug")
 
-    args = ['pelican', content_path, f'--settings={settings_module.__file__}', '--fatal=warnings']
+    args = [
+        "pelican",
+        content_path,
+        f"--settings={settings_module.__file__}",
+        "--fatal=warnings",
+    ]
     args += extra_args
-    click.secho(shlex.join(args), fg='green', bold=True)
-    click.echo('')
+    click.secho(shlex.join(args), fg="green", bold=True)
+    click.echo("")
     subprocess.run(args, check=True)
 
 
 @main.command()
-@click.argument('title')
-@click.option('--path', 'content_path', default='content', type=click.Path(exists=True, path_type=Path))
-@click.option('--debug/--no-debug', default=False)
-@click.option('--open/--no-open', default=True)
+@click.argument("title")
+@click.option(
+    "--path",
+    "content_path",
+    default="content",
+    type=click.Path(exists=True, path_type=Path),
+)
+@click.option("--debug/--no-debug", default=False)
+@click.option("--open/--no-open", default=True)
 def new(title, content_path, debug, open):
     today = date.today()
     today_iso = today.isoformat()
-    content = dedent(f'''
+    content = dedent(
+        f"""
         Title: {title}
         Image: images/???.jpg
         Lang: cs
@@ -77,11 +88,12 @@ def new(title, content_path, debug, open):
         Fotka od [???](https://unsplash.com/@???)
 
         Něco dalšího.
-    ''').lstrip()
-    path = content_path / f'{today_iso}_{slugify(title)}.md'
+    """
+    ).lstrip()
+    path = content_path / f"{today_iso}_{slugify(title)}.md"
     if debug:
         click.secho(path.name, bold=True)
-        click.echo('')
+        click.echo("")
         click.echo(content)
     else:
         path.write_text(content)
@@ -90,45 +102,62 @@ def new(title, content_path, debug, open):
 
 
 @main.command()
-@click.option('--path', 'content_path', default='content', type=click.Path(exists=True))
-@click.option('--articles', 'articles_count', default=1, type=int)
-@click.option('--settings-module', default='pelicanconf', type=importlib.import_module)
-@click.option('--wait', 'wait_sec', default=10, type=int)
-@click.option('--open/--no-open', 'open_browser', default=True)
-@click.option('--cache/--no-cache', default=True)
-@click.option('--debug/--no-debug', default=False)
+@click.option("--path", "content_path", default="content", type=click.Path(exists=True))
+@click.option("--articles", "articles_count", default=1, type=int)
+@click.option("--settings-module", default="pelicanconf", type=importlib.import_module)
+@click.option("--wait", "wait_sec", default=10, type=int)
+@click.option("--open/--no-open", "open_browser", default=True)
+@click.option("--cache/--no-cache", default=True)
+@click.option("--debug/--no-debug", default=False)
 @click.pass_context
-def dev(context, content_path, articles_count, settings_module,
-        wait_sec, open_browser, cache, debug):
+def dev(
+    context,
+    content_path,
+    articles_count,
+    settings_module,
+    wait_sec,
+    open_browser,
+    cache,
+    debug,
+):
     context.invoke(media)
 
     extra_args = []
 
     if debug:
-        extra_args.append('--debug')
+        extra_args.append("--debug")
     if not cache:
-        extra_args.append('--ignore-cache')
+        extra_args.append("--ignore-cache")
 
-    paths = sorted(Path(content_path).glob('*.md'), reverse=True)
+    paths = sorted(Path(content_path).glob("*.md"), reverse=True)
     if len(paths) > articles_count:
-        paths_json = json.dumps([str(article.relative_to(content_path))
-                                 for article in paths[:articles_count]])
-        extra_args.extend(['-e', f'ARTICLE_PATHS={paths_json}'])
+        paths_json = json.dumps(
+            [
+                str(article.relative_to(content_path))
+                for article in paths[:articles_count]
+            ]
+        )
+        extra_args.extend(["-e", f"ARTICLE_PATHS={paths_json}"])
 
     proc = None
     if open_browser:
         path = paths[0]
-        slug = re.search(settings_module.FILENAME_METADATA, path.stem).group('slug')
+        slug = re.search(settings_module.FILENAME_METADATA, path.stem).group("slug")
         output_path = settings_module.ARTICLE_URL.format(slug=slug)
-        proc = multiprocessing.Process(target=dev_open,
-                                       args=(output_path, wait_sec))
+        proc = multiprocessing.Process(target=dev_open, args=(output_path, wait_sec))
         proc.start()
 
-    args = ['pelican', content_path, '--fatal=errors',
-            '--listen', '--autoreload', '--bind=localhost']
+    args = [
+        "pelican",
+        content_path,
+        "--fatal=errors",
+        "--listen",
+        "--autoreload",
+        "--bind=localhost",
+    ]
     args += extra_args
-    click.secho(shlex.join(args), fg='green', bold=True)
-    click.echo('')
+    click.secho(shlex.join(args), fg="green", bold=True)
+    click.echo("")
     try:
         subprocess.run(args, check=True)
     finally:
@@ -141,8 +170,8 @@ def dev_open(path, wait_sec):
     click.launch(f"http://localhost:8000/{path}")
 
 
-@main.command(context_settings={'ignore_unknown_options': True})
-@click.argument('pytest_args', nargs=-1, type=click.UNPROCESSED)
+@main.command(context_settings={"ignore_unknown_options": True})
+@click.argument("pytest_args", nargs=-1, type=click.UNPROCESSED)
 def test(pytest_args):
     code = pytest.main(list(pytest_args))
     if code:
---
+++ blog/notion.py
@@ -0,0 +1,73 @@
+import click
+from notion_client import Client
+from notion_client.helpers import iterate_paginated_api
+
+from feedgen.feed import FeedGenerator
+
+
+NOTION_DATABASE_ID = "6300c201957f499a81ebc9a097950fc8"
+
+NOTION_FEED_ID = "EVVcNQWvn9.QMwn6Z--KMTk4rmJnvNyABKRCcGM@"
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+@click.argument("notion_token", envvar="NOTION_TOKEN")
+@click.option("--database-id", default=NOTION_DATABASE_ID)
+@click.option("--feed-id", default=NOTION_FEED_ID)
+def reading(notion_token, database_id, feed_id):
+    feed = FeedGenerator()
+    feed.id(feed_id)
+    feed.title("HJ's Reading")
+    notion = Client(auth=notion_token)
+    for result in iterate_paginated_api(
+        notion.databases.query, database_id=database_id
+    ):
+        for item in result:
+            url = item["properties"]["URL"]["url"]
+            if is_youtube_url(url):
+                continue
+
+            title = item["properties"]["Name"]["title"][0]["plain_text"]
+            created_at = item["created_time"]
+            entry = feed.add_entry()
+            entry.id(url)
+            entry.title(title)
+            entry.link(href=url)
+            entry.published(created_at)
+    click.echo(feed.atom_str())
+
+
+@main.command()
+@click.argument("notion_token", envvar="NOTION_TOKEN")
+@click.option("--database-id", default=NOTION_DATABASE_ID)
+@click.option("--feed-id", default=NOTION_FEED_ID)
+def watching(notion_token, database_id, feed_id):
+    feed = FeedGenerator()
+    feed.id(feed_id)
+    feed.title("HJ's Watching")
+    notion = Client(auth=notion_token)
+    for result in iterate_paginated_api(
+        notion.databases.query, database_id=database_id
+    ):
+        for item in result:
+            url = item["properties"]["URL"]["url"]
+            if not is_youtube_url(url):
+                continue
+
+            title = item["properties"]["Name"]["title"][0]["plain_text"]
+            created_at = item["created_time"]
+            entry = feed.add_entry()
+            entry.id(url)
+            entry.title(title)
+            entry.link(href=url)
+            entry.published(created_at)
+    click.echo(feed.atom_str())
+
+
+def is_youtube_url(url: str) -> bool:
+    return "youtube.com" in url or "youtu.be" in url
